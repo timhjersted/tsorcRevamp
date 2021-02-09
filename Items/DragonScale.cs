56994d442729e8a8ee178994fbed3f8d866a7672
Zeodexic <pieisgood48@gmail.com>
2021-02-09 15:26:18 -0500

    new guardian soul sprite, added dragon scales

---
+++ Items/DragonScale.cs
@@ -0,0 +1,15 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace tsorcRevamp.Items {
+    class DragonScale : ModItem {
+        public override void SetStaticDefaults() {
+            Tooltip.SetDefault("Used to craf tthe Stone of Seath, among other things");
+        }
+
+        public override void SetDefaults() {
+            item.rare = ItemRarityID.Orange;
+        }
+    }
+}
