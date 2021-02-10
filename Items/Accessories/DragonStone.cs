88c539336da6970bfd8be1bce7ab4ba34dc5c6ae
Zeodexic <pieisgood48@gmail.com>
2021-02-09 22:40:12 -0500

    remote got wiped for some reason. thank you github

---
+++ Items/Accessories/DragonStone.cs
@@ -0,0 +1,44 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace tsorcRevamp.Items.Accessories {
+    public class DragonStone : ModItem {
+        public override void SetStaticDefaults() {
+            Tooltip.SetDefault("Tooltip1" +
+                                "\nTooltip2" +
+                                "\nTooltip3" +
+                                "\nTooltip4" +
+                                "\nTooltip5");
+        }
+
+        public override void SetDefaults() {
+            item.width = 26;
+            item.height = 26;
+            item.accessory = true;
+            item.value = 375000;
+            item.rare = ItemRarityID.Pink;
+        }
+
+        public override void AddRecipes() {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.Bone, 69);
+            recipe.AddIngredient(mod.GetItem("DarkSoul"), 1337);
+            recipe.AddTile(TileID.DemonAltar);
+            recipe.SetResult(this, 1);
+            recipe.AddRecipe();
+        }
+
+        public override void UpdateEquip(Player player) {
+            player.meleeDamage -= 2f;
+            player.meleeCrit = -50;
+            if (player.statLife <= 80) {
+                player.statDefense += 85;
+            }
+            else {
+                player.statDefense += 30;
+            }
+        }
+
+    }
+}
