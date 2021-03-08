b28b859c0654c0466b73162ac2410a28632bf6de
Zeodexic <pieisgood48@gmail.com>
2021-03-07 21:37:57 -0500

    ephemeral dust

---
+++ Items/EphemeralDust.cs
@@ -0,0 +1,15 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace tsorcRevamp.Items {
+    class EphemeralDust : ModItem {
+
+        public override void SetDefaults() {
+            item.width = 16;
+            item.height = 14;
+            item.rare = ItemRarityID.Blue;
+            item.value = 1000;
+            item.maxStack = 250;
+        }
+    }
+}
