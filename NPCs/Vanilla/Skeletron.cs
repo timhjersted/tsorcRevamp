18578e9a20930f37f42e4dbd76b427102cbc4fe2
Zeodexic <pieisgood48@gmail.com>
2021-03-25 11:00:14 -0400

    actually push the skeletron fix lol

--- NPCs/Vanilla/Skeletron.cs
+++ NPCs/Vanilla/Skeletron.cs
@@ -5,8 +5,8 @@ using Terraria.ModLoader;
 namespace tsorcRevamp.NPCs {
     class Skeletron : GlobalNPC {
         public override bool PreAI(NPC npc) { //todo
-            if ((npc.type == NPCID.SkeletronHead || npc.type == NPCID.SkeletronHand) && ModContent.GetInstance<tsorcRevampConfig>().RenameSkeletron) { //i am well aware that it says "Skeletron has awoken!" if Skeletron is 
-                npc.GivenName = "Gravelord Nito"; //spawned via the old man / clothier. i am not familiar enough with IL editing to fix that. 
+            if ((npc.type == NPCID.SkeletronHead || npc.type == NPCID.SkeletronHand) && ModContent.GetInstance<tsorcRevampConfig>().RenameSkeletron) {
+                npc.GivenName = "Gravelord Nito"; //this will not replace the text written to chat. that is handled in tsorcRevamp.cs
             }
             return true;
         }
