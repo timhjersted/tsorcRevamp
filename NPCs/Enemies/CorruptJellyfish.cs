f3f71db94db7d97e51f66404d4a9138f458d1cf6
Chroma-Equinox <55064035+Chroma-Equinox@users.noreply.github.com>
2021-03-26 23:12:17 +0100

    Corrupted Jellyfish + Armored Wraith

--- NPCs/Enemies/CorruptJellyfish.cs
+++ NPCs/Enemies/CorruptJellyfish.cs
@@ -5,42 +5,46 @@ using Microsoft.Xna.Framework;
 
 namespace tsorcRevamp.NPCs.Enemies
 {
-    class CloudBat : ModNPC
+    class CorruptedJellyfish : ModNPC
     {
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Cloud Bat");
-            Main.npcFrameCount[npc.type] = Main.npcFrameCount[NPCID.GiantBat];
+            DisplayName.SetDefault("Corrupt Jellyfish");
+            Main.npcFrameCount[npc.type] = Main.npcFrameCount[NPCID.PinkJellyfish];
         }
 
         public override void SetDefaults()
         {
-            npc.CloneDefaults(NPCID.CaveBat);
-            animationType = NPCID.GiantBat;
-            aiType = NPCID.CaveBat;
-            npc.lifeMax = 70;
-            npc.damage = 90;
+            npc.CloneDefaults(NPCID.PinkJellyfish);
+            animationType = NPCID.PinkJellyfish;
+            aiType = NPCID.PinkJellyfish;
+            npc.lifeMax = 235;
+            npc.damage = 70;
             npc.scale = 1f;
-            npc.knockBackResist = .55f;
-            npc.value = 350;
-            npc.defense = 5;
+            npc.knockBackResist = .7f;
+            npc.value = 380;
+            npc.defense = 40;
             npc.buffImmune[BuffID.Confused] = true;
         }
         public override float SpawnChance(NPCSpawnInfo spawnInfo)
         {
             float chance = 0;
-            if (Main.hardMode)
+            if (Main.hardMode /*|| ModWorld.superHardmode*/)
             {
-                chance = SpawnCondition.Sky.Chance * 0.2f; 
+                if ((spawnInfo.player.ZoneCorrupt && spawnInfo.water) && (spawnInfo.player.ZoneRockLayerHeight || spawnInfo.player.ZoneDirtLayerHeight)) //I've added dirt layer as otherwise they would be practically non-existent on the tsorc map
+                {
+                    chance = .25f;
+                }
             }
             return chance;
         }
 
         public override void OnHitPlayer(Player target, int damage, bool crit)
         {
-            if (Main.rand.Next(8) == 0)
+            if (Main.rand.Next(4) == 0)
             {
-                target.AddBuff(BuffID.Confused, 600, true);
+                target.AddBuff(BuffID.ObsidianSkin, 1800, true);
+                target.AddBuff(BuffID.Bleeding, 3600, true);
             }
         }
         public override void NPCLoot()
@@ -51,7 +55,7 @@ namespace tsorcRevamp.NPCs.Enemies
         {
             for (int i = 0; i < 15; i++)
             {
-                int dustType = 16;
+                int dustType = 98;
                 int dustIndex = Dust.NewDust(npc.position, npc.width, npc.height, dustType);
                 Dust dust = Main.dust[dustIndex];
                 dust.velocity.X = dust.velocity.X + Main.rand.Next(-50, 51) * 0.04f;
@@ -63,9 +67,23 @@ namespace tsorcRevamp.NPCs.Enemies
             {
                 for (int i = 0; i < 30; i++)
                 {
-                    Dust.NewDust(npc.position, npc.width, npc.height, 54, Main.rand.Next(-10, 10), Main.rand.Next(-10, 10), 70, default(Color), .8f);
+                    Dust.NewDust(npc.position, npc.width, npc.height, 98, Main.rand.Next(-10, 10), Main.rand.Next(-10, 10), 70, default(Color), .8f);
                 }
             }
         }
+
+        //TO-DO
+        /*public void AI()
+        {
+            for (int i = 0; i < npc.buffType.Length; i++)
+            {
+                if (npc.buffType[i] == Config.buffID["Frozen"])
+                {
+                    npc.DelBuff(i);
+                    i = 0;
+                }
+            }
+            npc.AI(true);
+        }*/
     }
 }
