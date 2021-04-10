b762dfd9fd978276a05ae9dd656418946e46f523
Chroma-Equinox <55064035+Chroma-Equinox@users.noreply.github.com>
2021-04-11 00:03:27 +0200

    Cobalt Halberd, Old and Reforged Halberd now work like spears or swung weapons based on L/R click

--- Items/Weapons/Melee/CobaltHalberd.cs
+++ Items/Weapons/Melee/CobaltHalberd.cs
@@ -1,37 +1,35 @@
-﻿using Terraria;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace tsorcRevamp.Items.Weapons.Melee
-{
-    class ReforgedOldHalberd : ModItem
+namespace tsorcRevamp.Items.Weapons.Melee {
+    public class CobaltHalberd : ModItem
     {
-        public override void SetStaticDefaults()
-        {
-            Tooltip.SetDefault("Does random damage from 0 to 35" +
-                                "\nMaximum damage is increased by damage modifiers." +
-                                "\nLeft-click to stab like a spear, right-click to swing" +
-                                "\nSwing strike does 25% more damage");
+	public override void SetStaticDefaults()
+	{
+		DisplayName.SetDefault("Cobalt Halberd");
+        Tooltip.SetDefault("Left-click to stab like a spear, right-click to swing" +
+                            "\nSwing strike does 25% more damage");
 
-        }
+	}
 
         public override void SetDefaults()
         {
-            item.damage = 14;
-            item.width = 68;
-            item.height = 68;
-            item.knockBack = 6;
+            item.rare = ItemRarityID.LightRed;
+            item.damage = 29;
+            item.width = 76;
+            item.height = 74;
+            item.knockBack = (float)6;
             item.maxStack = 1;
             item.melee = true;
-            item.scale = 1f;
+            item.autoReuse = false;
+            item.scale = 1;
             item.useAnimation = 28;
-            item.rare = ItemRarityID.White;
             item.UseSound = SoundID.Item1;
             item.useStyle = ItemUseStyleID.SwingThrow;
             item.useTime = 28;
-            item.value = 7000;
+            item.value = 69000;
         }
-
         public override bool AltFunctionUse(Player player)
         {
             return true;
@@ -46,17 +44,16 @@ namespace tsorcRevamp.Items.Weapons.Melee
                 item.shoot = ProjectileID.None;
                 item.noMelee = false;
                 item.noUseGraphic = false;
-                item.damage = 18;
+                item.damage = 36;
             }
             else
             {
+                item.damage = 29;
                 item.noMelee = true;
                 item.noUseGraphic = true;
                 item.useStyle = ItemUseStyleID.HoldingOut;
-                item.shoot = ModContent.ProjectileType<Projectiles.OldHalberd>();
+                item.shoot = ModContent.ProjectileType<Projectiles.CobaltHalberd>();
                 item.shootSpeed = 2.7f;
-                item.damage = 14;
-
             }
             return base.CanUseItem(player);
         }
@@ -64,11 +61,15 @@ namespace tsorcRevamp.Items.Weapons.Melee
         public override void AddRecipes()
         {
             ModRecipe recipe = new ModRecipe(mod);
-            recipe.AddIngredient(mod.GetItem("OldHalberd"));
-            recipe.AddTile(mod.GetTile("SweatyCyclopsForge"));
-            recipe.SetResult(this);
-            recipe.AddRecipe();
 
+            recipe.AddIngredient(ItemID.CobaltBar, 10);
+            recipe.AddIngredient(mod.GetItem("DarkSoul"), 1000);
+
+            recipe.AddTile(TileID.DemonAltar);
+            recipe.SetResult(this, 1);
+            recipe.AddRecipe();
         }
+
+
     }
 }
--- Items/Weapons/Melee/OldHalberd.cs
+++ Items/Weapons/Melee/OldHalberd.cs
@@ -9,7 +9,8 @@ namespace tsorcRevamp.Items.Weapons.Melee
         public override void SetStaticDefaults()
         {
             Tooltip.SetDefault("Does random damage from 0 to 35" +
-                                "\nMaximum damage is increased by damage modifiers.");
+                                "\nMaximum damage is increased by damage modifiers." +
+                                "\nLeft-click to stab like a spear, right-click to swing");
         }
 
         public override void SetDefaults()
@@ -27,6 +28,32 @@ namespace tsorcRevamp.Items.Weapons.Melee
             item.useStyle = ItemUseStyleID.SwingThrow;
             item.useTime = 21;
             item.value = 7000;
+        }
+
+        public override bool AltFunctionUse(Player player)
+        {
+            return true;
+        }
+
+        public override bool CanUseItem(Player player)
+        {
+            if (player.altFunctionUse == 2)
+            {
+
+                item.useStyle = ItemUseStyleID.SwingThrow;
+                item.shoot = ProjectileID.None;
+                item.noMelee = false;
+                item.noUseGraphic = false;
+            }
+            else
+            {
+                item.noMelee = true;
+                item.noUseGraphic = true;
+                item.useStyle = ItemUseStyleID.HoldingOut;
+                item.shoot = ModContent.ProjectileType<Projectiles.OldHalberd>();
+                item.shootSpeed = 2.7f;
+            }
+            return base.CanUseItem(player);
         }
 
         public override void HoldItem(Player player)
