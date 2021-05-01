66cee6b4cff15d5f20d3e49343f5096fb7dfa1f0
Chroma-Equinox <55064035+Chroma-Equinox@users.noreply.github.com>
2021-05-01 19:12:11 +0200

    Bonfire UI Elements

---
+++ UI/ButtonSafe.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.UI;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace tsorcRevamp.UI
+{
+    class ButtonSafe : UIElement
+    {
+        //Color color = new Color(50, 255, 153);
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(ModContent.GetTexture("tsorcRevamp/UI/ButtonSafe"), new Vector2(Main.screenWidth + 90, Main.screenHeight - 20) / 2f, default);
+        }
+    }
+}
