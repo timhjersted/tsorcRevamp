4875fe8efd0fc7888086f41a8e9a2d7d281a4e35
Zeodexic <pieisgood48@gmail.com>
2021-05-08 19:42:33 -0400

    temporarily remove projectiles

---
+++ Buffs/Hold.cs
@@ -0,0 +1,24 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace tsorcRevamp.Buffs {
+    class Hold : ModBuff {
+
+        public override void SetDefaults() {
+            DisplayName.SetDefault("Held");
+            Description.SetDefault("You're being held in position!");
+            Main.debuff[Type] = true;
+            Main.buffNoTimeDisplay[Type] = false;
+        }
+
+        public override void Update(Player player, ref int buffIndex) {
+            player.velocity.X = 0;
+            player.velocity.Y = 0;
+        }
+        public override void Update(NPC npc, ref int buffIndex) {
+            npc.velocity.X = 0;
+            npc.velocity.Y = 0;
+        }
+    }
+}
