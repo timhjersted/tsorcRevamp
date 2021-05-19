6b53ad95c40dae82c4aea3cdc72108b12b40f7b8
Zeodexic <pieisgood48@gmail.com>
2021-05-19 02:46:02 -0400

    woops THIS one is the gem robe fix

--- Items/VanillaItems/RecipeHelper.cs
+++ Items/VanillaItems/RecipeHelper.cs
@@ -6,6 +6,7 @@ namespace tsorcRevamp {
     public static class RecipeHelper {
 
         public static void RecipeRemover(int ItemRecipeToRemove) {
+            //removes ANY recipe that results in ItemRecipeToRemove
                 RecipeFinder finder = new RecipeFinder();
                 finder.SetResult(ItemRecipeToRemove);
 
@@ -17,6 +18,7 @@ namespace tsorcRevamp {
         }
 
         public static void RecipeIngredientAdder(int ItemRecipeToEdit, int ItemIngredientToAdd, int ItemCount = 1) {
+            //any recipe that results in ItemRecipeToEdit will have ItemIngredientToAdd added to it, with ItemCount amount (default 1)
             RecipeFinder finder = new RecipeFinder();
             finder.SetResult(ItemRecipeToEdit);
 
@@ -27,6 +29,9 @@ namespace tsorcRevamp {
         }
 
         public static void ExactRecipeRemover2Ingredients(int Ingredient1, int Ingredient1Amount, int Ingredient2, int Ingredient2Amount, int CraftingStation, int RecipeResult) {
+            //this method is for when there's an item whose recipe needs to be removed, but we can't use RecipeRemover
+            //that usually means we're giving it a custom recipe somewhere else, since RecipeRemover runs on any recipe that results in that item
+            //using exact recipes is thus required. not sure if we need to do this again, but if we do, now theres a method
             RecipeFinder finder = new RecipeFinder();
             finder.AddIngredient(Ingredient1, Ingredient1Amount);
             finder.AddIngredient(Ingredient2, Ingredient2Amount);
@@ -52,6 +57,8 @@ namespace tsorcRevamp {
             RecipeRemover(ItemID.RopeCoil);
             RecipeRemover(ItemID.VineRopeCoil);
             RecipeRemover(ItemID.WebRopeCoil);
+            RecipeRemover(ItemID.SilkRopeCoil);
+
             RecipeIngredientAdder(ItemID.IvyWhip, ItemID.SoulofNight, 6);
             RecipeIngredientAdder(ItemID.GrapplingHook, ItemID.SoulofNight, 6);
             RecipeIngredientAdder(ItemID.AmethystHook, ItemID.SoulofNight, 6);
@@ -60,6 +67,14 @@ namespace tsorcRevamp {
             RecipeIngredientAdder(ItemID.EmeraldHook, ItemID.SoulofNight, 6);
             RecipeIngredientAdder(ItemID.RubyHook, ItemID.SoulofNight, 6);
             RecipeIngredientAdder(ItemID.DiamondHook, ItemID.SoulofNight, 6);
+
+            RecipeIngredientAdder(ItemID.AmethystRobe, ModContent.ItemType<Items.DarkSoul>(), 550);
+            RecipeIngredientAdder(ItemID.TopazRobe, ModContent.ItemType<Items.DarkSoul>(), 600);
+            RecipeIngredientAdder(ItemID.SapphireRobe, ModContent.ItemType<Items.DarkSoul>(), 650);
+            RecipeIngredientAdder(ItemID.EmeraldRobe, ModContent.ItemType<Items.DarkSoul>(), 700);
+            RecipeIngredientAdder(ItemID.RubyRobe, ModContent.ItemType<Items.DarkSoul>(), 750);
+            RecipeIngredientAdder(ItemID.DiamondRobe, ModContent.ItemType<Items.DarkSoul>(), 800);
+
             ExactRecipeRemover2Ingredients(ItemID.Hellstone, 3, ItemID.BottledWater, 1, TileID.ImbuingStation, ItemID.FlaskofFire);
         }
     }
