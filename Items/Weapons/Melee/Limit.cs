1c2907a61f216f83c21ac45637857d69b88cc1a3
Zeodexic <pieisgood48@gmail.com>
2021-07-10 07:48:09 -0400

    first fractal --> limit. placeholder(?) sprite

--- Items/Weapons/Melee/Limit.cs
+++ Items/Weapons/Melee/Limit.cs
@@ -6,7 +6,7 @@ using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace tsorcRevamp.Items.Weapons.Melee {
-    class FirstFractal : ModItem {
+    class Limit : ModItem {
 
         public override void SetDefaults() {
             item.useStyle = ItemUseStyleID.HoldingOut;
@@ -18,11 +18,11 @@ namespace tsorcRevamp.Items.Weapons.Melee {
             item.melee = true;
             item.channel = true;
             item.noMelee = true;
-            item.shoot = ModContent.ProjectileType<Projectiles.FirstFractal>();
+            item.shoot = ModContent.ProjectileType<Projectiles.Limit>();
             item.useAnimation = 40;
             item.useTime = item.useAnimation / 4;
             item.shootSpeed = 0f;
-            item.damage = 190;
+            item.damage = 150;
             item.knockBack = 6.5f;
             item.value = Item.sellPrice(0, 20);
             item.crit = 10;
@@ -30,18 +30,15 @@ namespace tsorcRevamp.Items.Weapons.Melee {
             item.glowMask = 271;
         }
 
-        //the damn thing shoots further to the right than it does to the left
         public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack) {
             Vector2 mousePos = Main.MouseWorld;
             Vector2 playerToMouse = mousePos - player.Center;
 
-            if (playerToMouse.Length() > 150f) {
-                playerToMouse *= 150f / playerToMouse.Length();
+            if (playerToMouse.Length() > 75f) {
+                playerToMouse *= 75f / playerToMouse.Length();
                 mousePos = player.Center + playerToMouse;
             }
-            mousePos.X -= 8;
-            mousePos.Y -= 32;
-            Projectile.NewProjectile(mousePos, new Vector2(18,  0), ModContent.ProjectileType<Projectiles.FirstFractal>(), item.damage, item.knockBack, item.owner);
+            Projectile.NewProjectile(mousePos, new Vector2(18,  0), ModContent.ProjectileType<Projectiles.Limit>(), item.damage, item.knockBack, item.owner);
             return false;
         }
     }
--- Projectiles/Limit.cs
+++ Projectiles/Limit.cs
@@ -4,45 +4,49 @@ using Terraria;
 using Terraria.ModLoader;
 
 namespace tsorcRevamp.Projectiles {
-    class FirstFractal : ModProjectile {
+    class Limit : ModProjectile {
+		int flip;
+		int direction;
 
-        public override void SetDefaults() {
-			projectile.width = 32;
-			projectile.height = 32;
+		public override string Texture => "tsorcRevamp/Items/Weapons/Melee/Limit";
+		public override void SetDefaults() {
+			projectile.width = 43;
+			projectile.height = 43;
 			projectile.friendly = true;
 			projectile.melee = true;
 			projectile.tileCollide = false;
 			projectile.ignoreWater = true;
 			projectile.extraUpdates = 1;
 			projectile.usesLocalNPCImmunity = true;
-			projectile.manualDirectionChange = true;
+			projectile.localNPCHitCooldown = 8; //this thing is way stronger than it looks
 			projectile.penetrate = -1;
-			projectile.timeLeft = 150;
+			projectile.timeLeft = 90;
 		}
         
         public override void AI() {
+			if (projectile.ai[0] < 1) {
+				flip = Main.rand.Next(2);
+				direction = (flip == 0 ? 1 : -1); //choose a rotation direction when the projectile is spawned
+            }
 			projectile.ai[0]++;
-			projectile.rotation = (float)Math.PI / 4f * (float)projectile.spriteDirection + projectile.velocity.ToRotation();
-			if (projectile.spriteDirection == -1) {
-				projectile.rotation += (float)Math.PI;
+			projectile.rotation -= 0.225f * direction;
+			if (projectile.ai[0] < 45) {
+				projectile.velocity = Vector2.Zero;
 			}
-			if (projectile.ai[0] < 60) {
-				Vector2 velocity = projectile.velocity;
-				projectile.velocity = velocity.RotatedBy((float)Math.Atan(projectile.ai[0]) * 0.25);
-			}
-			
+
 			else if (projectile.ai[1] == 0) {
 				Vector2 speed;
 				speed.X = (Main.mouseX + Main.screenPosition.X) - (projectile.position.X + projectile.width * 0.5f);
 				speed.Y = (Main.mouseY + Main.screenPosition.Y) - (projectile.position.Y + projectile.height * 0.5f);
 				speed.Normalize();
-				speed *= 9f;
+				speed *= 5f;
 				projectile.velocity = speed;
 				projectile.ai[1] = 1;
 			}
-			if (projectile.ai[0] > 120) {
-				projectile.alpha += 8;
-            }
+			if (projectile.ai[0] > 75) {
+				projectile.alpha += 17;
+				
+			}
 		}
     }
 }
