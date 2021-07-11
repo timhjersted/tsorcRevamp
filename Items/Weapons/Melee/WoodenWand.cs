2fdab086893688e2bfeb9d9f8e9a2168af8c95c5
Zeodexic <34223911+Zeodexic@users.noreply.github.com>
2021-07-11 03:21:29 -0400

    wooden wand tooltip update

--- Items/Weapons/Melee/WoodenWand.cs
+++ Items/Weapons/Melee/WoodenWand.cs
@@ -4,7 +4,7 @@ using Terraria.ModLoader;
 namespace tsorcRevamp.Items.Weapons.Melee {
     class WoodenWand : ModItem {
         public override void SetStaticDefaults() {
-            Tooltip.SetDefault("Can be upgraded in 3 different ways with 150 or 2000 or (100 Crystal Shard + 6000) Dark Souls");
+            Tooltip.SetDefault("An unenchanted wooden wand. \nCan be upgraded many different ways.");
         }
 
         public override void SetDefaults() {
