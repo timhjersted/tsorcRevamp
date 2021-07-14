c75ae01c643e498c8cbc99087998a4af8d6e406e
RecursiveCollapse <recursivecollapse@protonmail.com>
2021-07-14 03:30:28 +0000

    Fixed forcibly drawing the shield
    Done by changing how the shadow effect was drawn

--- Items/Armors/WitchkingHelmet.cs
+++ Items/Armors/WitchkingHelmet.cs
@@ -35,13 +35,15 @@ namespace tsorcRevamp.Items.Armors
             player.lifeRegen += 3;
             player.nightVision = true;
             player.noFallDmg = true;
-            //player.ShadowAura = true;
-            player.eocDash = 20;
-            player.armorEffectDrawShadowEOCShield = true;
 
             int i2 = (int)(player.position.X + (float)(player.width / 2) + (float)(8 * player.direction)) / 16;
             int j2 = (int)(player.position.Y + 2f) / 16;
             Lighting.AddLight(i2, j2, 0.92f, 0.8f, 0.65f);
+        }
+        
+        public override void ArmorSetShadows (Player player)
+        {
+            player.armorEffectDrawShadow = true;
         }
     }
 }
