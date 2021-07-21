0f9a15bd8efc74eb6862106d1ac6ac94893e29f4
Chroma-Equinox <55064035+Chroma-Equinox@users.noreply.github.com>
2021-07-21 13:32:36 +0200

    Changed tooltip on Mana Cloak

--- Items/Accessories/ManaCloak.cs
+++ Items/Accessories/ManaCloak.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -7,8 +8,7 @@ namespace tsorcRevamp.Items.Accessories {
     public class ManaCloak : ModItem {
         public override void SetStaticDefaults() {
             Tooltip.SetDefault("Rapid mana regen, +15% magic crit & +15% magic dmg when health falls below 150\n" +
-                                "Provides Star cloak, +5 magic crit & +5% magic damage boost normally\n" +
-                                "Halves the duration of Magic Imbue Cooldown");
+                                "Provides Star cloak, +5 magic crit & +5% magic damage boost normally");
         }
 
         public override void SetDefaults() {
@@ -49,6 +49,20 @@ namespace tsorcRevamp.Items.Accessories {
                 player.starCloak = true;
                 player.magicCrit += 5;
                 player.magicDamage += .05f;
+            }
+        }
+
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        {
+            if (!ModContent.GetInstance<tsorcRevampConfig>().LegacyMode)
+            {
+                //only insert the tooltip if the last valid line is not the name, the "Equipped in social slot" line, or the "No stats will be gained" line (aka do not insert if in a vanity slot)
+                int ttindex = tooltips.FindLastIndex(t => t.mod == "Terraria" && t.Name != "ItemName" && t.Name != "Social" && t.Name != "SocialDesc");
+                if (ttindex != -1)
+                {// if we find one
+                    //insert the extra tooltip line
+                    tooltips.Insert(ttindex + 1, new TooltipLine(mod, "RevampManaCloak", "[c/00ff00:Revamped Mode:] Halves the duration of Magic Imbue Cooldown"));
+                }
             }
         }
     }
