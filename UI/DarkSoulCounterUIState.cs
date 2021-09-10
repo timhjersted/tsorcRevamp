6f77f9399c1f831e226a50b7e70e3bb2944e89ac
Zeodexic <34223911+Zeodexic@users.noreply.github.com>
2021-09-09 21:47:41 -0400

    movable soul counter (NOT DRAGGABLE)
    no dragging because i couldnt get it to work yet and im tired of having my ide open

--- UI/DarkSoulCounterUIState.cs
+++ UI/DarkSoulCounterUIState.cs
@@ -4,6 +4,9 @@ using Terraria;
 using Terraria.GameContent.UI.Elements;
 using Terraria.UI;
 using Terraria.ModLoader;
+using Microsoft.Xna.Framework.Input;
+using System.Reflection;
+using Terraria.ModLoader.Config;
 
 namespace tsorcRevamp.UI
 {
@@ -17,13 +20,16 @@ namespace tsorcRevamp.UI
 
 		public static bool Visible = true;
 
+		public static tsorcRevampConfig ConfigInstance = ModContent.GetInstance<tsorcRevampConfig>();
+		public static Vector2 DrawPos = new Vector2(ConfigInstance.SoulCounterPosX, ConfigInstance.SoulCounterPosY);
+
 		public override void OnInitialize()
 		{
 			// Create a UIElement for all the elements to sit on top of, this simplifies the numbers as nested elements can be positioned relative to the top left corner of this element. 
 			// UIElement is invisible and has no padding. You can use a UIPanel if you wish for a background.
 			area = new UIElement();
-			area.Left.Set(-area.Width.Pixels - 220, 1f); // Place the resource bar to the left of the hearts.
-			area.Top.Set(-area.Height.Pixels - 70, 1f); // Placing it just a bit below the top of the screen.
+			area.Left.Set(-(ConfigInstance.SoulCounterPosX), 1f); // Place the resource bar to the left of the hearts.
+			area.Top.Set(-(ConfigInstance.SoulCounterPosY), 1f); // Placing it just a bit below the top of the screen.
 			area.Width.Set(200, 0f); // We will be placing the following 2 UIElements within this 182x60 area.
 			area.Height.Set(50, 0f);
 
@@ -43,14 +49,28 @@ namespace tsorcRevamp.UI
 
 			Append(area);
 		}
-
-		public override void Update(GameTime gameTime)
+        public override void Update(GameTime gameTime)
 		{
+			if (Main.time % 30 == 0) {
+				Main.NewText("config pos " + ConfigInstance.SoulCounterPosX + ", " + ConfigInstance.SoulCounterPosY);
+				Main.NewText("set pos " + (-area.Left.Pixels) + ", " + -(area.Top.Pixels));
+            }
 			var modPlayer = Main.LocalPlayer.GetModPlayer<tsorcRevampPlayer>();
 			var item = ModContent.ItemType<Items.DarkSoul>();
 			// Setting the text per tick to update and show our DS values.
 			soulQuantityText.SetText($"[i:{item}]  [c/D3D3D3:{modPlayer.darkSoulQuantity}]");
+
+			if ((-area.Left.Pixels) != ConfigInstance.SoulCounterPosX) {
+				area.Left.Pixels = -ConfigInstance.SoulCounterPosX;
+			}
+
+			if ((-area.Top.Pixels) != ConfigInstance.SoulCounterPosY) {
+				area.Top.Pixels = -ConfigInstance.SoulCounterPosY;
+			}
+
 			base.Update(gameTime);
 		}
+
+
 	}
 }
