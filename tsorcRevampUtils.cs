6bdca7f8188688c5961bac5fa529987baa319106
Zeodexic <34223911+Zeodexic@users.noreply.github.com>
2021-09-23 23:15:42 -0400

    item price by rarity util

--- tsorcRevampUtils.cs
+++ tsorcRevampUtils.cs
@@ -98,4 +98,47 @@ namespace tsorcRevamp {
     public static class VariousConstants {
         public const int CUSTOM_MAP_WORLD_ID = 44874972;
     }
+
+    public static class PriceByRarity { 
+        
+        //minimal exploration. pre-hardmode ores. likely no items that craft from souls will use this
+        public static readonly int White_0 = Item.buyPrice(platinum: 0, gold: 0, silver: 40, copper: 0);
+
+        //underground chest loots (shoe spikes, CiaB, etc), shadow orb items, floating island
+        public static readonly int Blue_1 = Item.buyPrice(platinum: 0, gold: 2, silver: 50, copper: 0);
+
+        //gold dungeon chest (handgun, cobalt shield, etc), goblin invasion
+        public static readonly int Green_2 = Item.buyPrice(platinum: 0, gold: 5, silver: 0, copper: 0);
+
+        //hell, underground jungle
+        public static readonly int Orange_3 = Item.buyPrice(platinum: 0, gold: 10, silver: 0, copper: 0);
+
+        //early hardmode (hm ores), mimics
+        public static readonly int LightRed_4 = Item.buyPrice(platinum: 0, gold: 14, silver: 50, copper: 0);
+
+        //hallowed tier. post mech, pre plantera. pirates.
+        public static readonly int Pink_5 = Item.buyPrice(platinum: 0, gold: 25, silver: 50, copper: 0);
+
+        //some biome mimic gear, high level tinkerer combinations (ankh charm, mechanical glove). seldom used in vanilla
+        public static readonly int LightPurple_6 = Item.buyPrice(platinum: 0, gold: 37, silver: 50, copper: 0);
+
+        //plantera, golem, chlorophyte
+        public static readonly int Lime_7 = Item.buyPrice(platinum: 0, gold: 47, silver: 50, copper: 0);
+        
+        //post-plantera dungeon, martian madness, pumpkin/frost moon
+        public static readonly int Yellow_8 = Item.buyPrice(platinum: 0, gold: 55, silver: 0, copper: 0);
+        
+        //lunar fragments, dev armor. seldom used in vanilla
+        public static readonly int Cyan_9 = Item.buyPrice(platinum: 0, gold: 67, silver: 50, copper: 0);
+        
+        //luminite, lunar fragment gear, moon lord drops
+        public static readonly int Red_10 = Item.buyPrice(platinum: 0, gold: 80, silver: 0, copper: 0);
+        
+        //no vanilla items have purple rarity base. only cyan and red with modifiers can be purple. im guessing on the price.
+        public static readonly int Purple_11 = Item.buyPrice(platinum: 1, gold: 20, silver: 0, copper: 0);
+
+
+
+    }
+
 }
