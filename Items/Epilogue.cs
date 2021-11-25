efee75bdca3fa78f2016aef01523a1581211ed2f
timhjersted <88109305+timhjersted@users.noreply.github.com>
2021-11-24 18:17:50 -0600

    Tweaked 'true ending' text
    Elizabeth renamed Asha. Changed text to designate this the "true ending" of the game.

--- Items/Epilogue.cs
+++ Items/Epilogue.cs
@@ -5,13 +5,13 @@ namespace tsorcRevamp.Items {
     class Epilogue : ModItem {
         public override string Texture => "tsorcRevamp/Items/Weapons/Magic/DeathStrikeScroll";
         public override void SetStaticDefaults() {
-            Tooltip.SetDefault("Congratulations, Red, you have vanquished the final guardian of the Abyss... " +
-                               "\nHaving defeated Gwyn, the portal that Attraidies opened at the moment of his death has closed. " +
+            Tooltip.SetDefault("Congratulations! You have unlocked the true ending of the game!" +
+                               "\nHaving defeated Gwyn, the portal that Attraidies opened at the moment of his death has closed." +
                                "\nAt last, you can finally feel the energies of the corruption losing their hold on the world," +
                                "\nthe darkness receding back into the earth, growing in harmony with the light once again." +
-                               "\nYou think of Elizabeth, waiting for you, and rejoice upon the thought of your return home." +
+                               "\nYou think of Asha, waiting for you, and rejoice upon the thought of your return home." +
                                "\nYou take a moment to think about all that you have been through... Forever you shall be known" +
-                               "\nas the hero of the age. Centuries from now, elders will tell your story. The story of Red Cloud.");
+                               "\nas the hero of the age. Centuries from now, elders will be telling your story. The story of Red Cloud.");
         }
 
         public override void SetDefaults() {
