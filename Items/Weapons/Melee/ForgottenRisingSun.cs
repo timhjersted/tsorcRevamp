284e20f8147572bd713b5aa2103570b7be64e1d7
Zeodexic <34223911+Zeodexic@users.noreply.github.com>
2021-11-28 14:35:58 -0500

    forgotten rising sun nerf (-15%)
    oopsie woopsie i made these things too fucky wuckying strong

--- Items/Weapons/Melee/ForgottenRisingSun.cs
+++ Items/Weapons/Melee/ForgottenRisingSun.cs
@@ -11,19 +11,20 @@ namespace tsorcRevamp.Items.Weapons.Melee {
             item.width = 22;
             item.height = 22;
             item.useStyle = ItemUseStyleID.SwingThrow;
-            item.useAnimation = 15;
-            item.useTime = 15;
+            item.useAnimation = 14;
+            item.useTime = 14;
             item.autoReuse = true;
-            item.maxStack = 10;
-            item.damage = 248;
+            item.maxStack = 1;
+            item.damage = 199;
             item.knockBack = 5;
             item.UseSound = SoundID.Item1;
             item.shootSpeed = 21;
             item.noUseGraphic = true;
             item.noMelee = true;
-            item.value = 17000;
+            item.value = PriceByRarity.Red_10;
             item.melee = true;
             item.shoot = ModContent.ProjectileType<Projectiles.ForgottenRisingSun>();
+            item.rare = ItemRarityID.Red;
         }
 
         public override bool CanUseItem(Player player) {
