c010dc8e8104cada7b296cca45884c144c9c5412
timhjersted <88109305+timhjersted@users.noreply.github.com>
2021-12-02 23:26:19 -0600

    harder to get cursed (-100 life)
    from 200 to 500

--- Buffs/PowerfulCurseBuildup.cs
+++ Buffs/PowerfulCurseBuildup.cs
@@ -13,13 +13,13 @@ namespace tsorcRevamp.Buffs {
         }
 
         public override void ModifyBuffTip(ref string tip, ref int rare) {
-            tip = "When the counter reaches 200, something terrible happens. Curse buildup is at " + Main.LocalPlayer.GetModPlayer<tsorcRevampPlayer>().PowerfulCurseLevel;
+            tip = "When the counter reaches 500, something terrible happens. Curse buildup is at " + Main.LocalPlayer.GetModPlayer<tsorcRevampPlayer>().PowerfulCurseLevel;
         }
         public override void Update(Player player, ref int buffIndex) {
-            if ((200 <= player.GetModPlayer<tsorcRevampPlayer>().PowerfulCurseLevel && player.GetModPlayer<tsorcRevampPlayer>().PowerfulCurseLevel <= 213) && player.statLifeMax > 100) { //if curse is between 200 and 213 inclusive, and the player has max hp to lose
+            if ((500 <= player.GetModPlayer<tsorcRevampPlayer>().PowerfulCurseLevel && player.GetModPlayer<tsorcRevampPlayer>().PowerfulCurseLevel <= 513) && player.statLifeMax > 100) { //if curse is between 500 and 513 inclusive, and the player has max hp to lose
                 player.statLifeMax -= 100;
                 Main.NewText("You have been cursed! -100 HP!");
-                player.GetModPlayer<tsorcRevampPlayer>().PowerfulCurseLevel = 214; //set it to just above bounds. dont curse the player twice for one curse event.
+                player.GetModPlayer<tsorcRevampPlayer>().PowerfulCurseLevel = 514; //set it to just above bounds. dont curse the player twice for one curse event.
                 player.AddBuff(ModContent.BuffType<Invincible>(), 1200); //20 seconds
                 player.AddBuff(ModContent.BuffType<Strength>(), 7200); //2 minutes
                 player.AddBuff(ModContent.BuffType<CrimsonDrain>(), 10800); //3 minutes
