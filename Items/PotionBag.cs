5eb27ca33c86832d3a2e66932d3ebcf4d3b8ed60
RecursiveCollapse <recursivecollapse@protonmail.com>
2022-01-08 15:39:38 -0500

    Added potion bag recipe
    Just for players with runs in-progress who didn't start with one

--- Items/PotionBag.cs
+++ Items/PotionBag.cs
@@ -120,5 +120,15 @@ namespace tsorcRevamp.Items {
             return false;
         }
 
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.Leather, 5);
+            recipe.AddIngredient(mod.GetItem("DarkSoul"), 75);
+            recipe.AddTile(TileID.DemonAltar);
+            recipe.SetResult(this, 1);
+            recipe.AddRecipe();
+        }
+
     }
 }
