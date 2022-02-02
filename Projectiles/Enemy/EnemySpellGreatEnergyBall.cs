92584d3c2cf8b21f15fe8fbb1621a6e866eebeaf
RecursiveCollapse <recursivecollapse@protonmail.com>
2022-02-02 03:47:41 -0500

    Moved more enemies to new AI system

--- Projectiles/Enemy/EnemySpellGreatEnergyBall.cs
+++ Projectiles/Enemy/EnemySpellGreatEnergyBall.cs
@@ -7,7 +7,8 @@ namespace tsorcRevamp.Projectiles.Enemy {
     class EnemySpellGreatEnergyBall : ModProjectile {
         public override string Texture => "tsorcRevamp/Projectiles/Bolt1Ball";
         public override void SetDefaults() {
-            projectile.aiStyle = 4;
+            projectile.aiStyle = 1;
+            projectile.timeLeft = 100;
             projectile.width = 16;
             projectile.height = 16;
             projectile.hostile = true;
@@ -16,26 +17,15 @@ namespace tsorcRevamp.Projectiles.Enemy {
             projectile.ignoreWater = true;
         }
         public override void Kill(int timeLeft) {
-            if (!projectile.active) {
-                return;
-            }
-            projectile.timeLeft = 0;
-            {
-                Main.PlaySound(SoundID.Item, (int)projectile.position.X, (int)projectile.position.Y, 10);
-                Projectile.NewProjectile(projectile.position.X + (float)(projectile.width / 2), projectile.position.Y + (float)(projectile.height - 16), 0, 0, ModContent.ProjectileType<EnemySpellGreatEnergyStrike>(), projectile.damage, 3f, projectile.owner);
-                Vector2 projectilePos = new Vector2(projectile.position.X - projectile.velocity.X, projectile.position.Y - projectile.velocity.Y);
-                int num41 = Dust.NewDust(projectilePos, projectile.width, projectile.height, 15, 0f, 0f, 100, default, 2f);
-                Main.dust[num41].noGravity = true;
-                Main.dust[num41].velocity *= 2f;
-                Dust.NewDust(projectilePos, projectile.width, projectile.height, 15, 0f, 0f, 100, default, 1f);
-                
-            }
-            if (projectile.owner == Main.myPlayer) {
-                if (Main.netMode != NetmodeID.SinglePlayer) {
-                    NetMessage.SendData(MessageID.KillProjectile, -1, -1, null, projectile.identity, (float)projectile.owner, 0f, 0f, 0);
-                }
-            }
-            projectile.active = false;
+           
+            Main.PlaySound(SoundID.Item, (int)projectile.position.X, (int)projectile.position.Y, 10);
+            Projectile.NewProjectile(projectile.position.X + (float)(projectile.width / 2), projectile.position.Y + (float)(projectile.height - 16), 0, 0, ModContent.ProjectileType<EnemySpellGreatEnergyStrike>(), projectile.damage, 3f, projectile.owner);
+            Vector2 projectilePos = new Vector2(projectile.position.X - projectile.velocity.X, projectile.position.Y - projectile.velocity.Y);
+            int num41 = Dust.NewDust(projectilePos, projectile.width, projectile.height, 15, 0f, 0f, 100, default, 2f);
+            Main.dust[num41].noGravity = true;
+            Main.dust[num41].velocity *= 2f;
+            Dust.NewDust(projectilePos, projectile.width, projectile.height, 15, 0f, 0f, 100, default, 1f);                
+            
         }
 
     }
