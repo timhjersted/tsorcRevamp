180039651addaaf5c96897c38589c4d81a630979
RecursiveCollapse <recursivecollapse@protonmail.com>
2022-07-11 11:29:37 -0400

    A word

--- Items/Armors/BlackBeltGiTop.cs
+++ Items/Armors/BlackBeltGiTop.cs
@@ -9,7 +9,7 @@ namespace tsorcRevamp.Items.Armors
     {
         public override void SetStaticDefaults()
         {
-            Tooltip.SetDefault("Adds Double Jump & Jump Boost Skills\nYou are a master of the zen arts, at one with the Tao\nSet Bonus: +20% Melee damage, +20% Melee Speed, +7% Melee Crit\nZen meditation adds amazing +13 Life Regen if ur defense is below 30");
+            Tooltip.SetDefault("Adds Double Jump & Jump Boost Skills\nYou are a master of the zen arts, at one with the Tao\nSet Bonus: +20% Melee damage, +20% Melee Speed, +7% Melee Crit\nZen meditation adds amazing +13 Life Regen when defense is below 30");
         }
 
         public override void SetDefaults()
