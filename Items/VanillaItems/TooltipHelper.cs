584e3c6cdc9b0ddaff793142fa29d0063c59407c
RecursiveCollapse <recursivecollapse@protonmail.com>
2022-07-15 12:21:49 -0400

    Updated outdated tooltips
    Also removed dark soul numbers from each, so we don't have to update it every time we change soul costs

--- Items/VanillaItems/TooltipHelper.cs
+++ Items/VanillaItems/TooltipHelper.cs
@@ -46,25 +46,24 @@ namespace tsorcRevamp.Items.VanillaItems
             //SimpleModTooltip(mod, item, tooltips, ItemID., "a");
             //SimpleModTooltip(mod, item, tooltips, ItemID., "a", "b");
             //SimpleModTooltip(mod, item, tooltips, ItemID.FlaskofFire, "Adds 10% melee damage");  don't do this. flask of fire's tooltip goes at a specific index, not the end
-            SimpleModTooltip(Mod, item, tooltips, ItemID.AdamantiteBreastplate, "Set can be upgraded in 3 ways, with 4000 Dark Souls for each piece");
+            SimpleModTooltip(Mod, item, tooltips, ItemID.AdamantiteBreastplate, "Set can be upgraded in 3 ways with Dark Souls");
             SimpleModTooltip(Mod, item, tooltips, ItemID.AdamantiteDrill, "Use this to open the Adamantite gates in the", "Corruption Temple to the west of the village");
-            SimpleModTooltip(Mod, item, tooltips, ItemID.AngelWings, "You will discover these in time...", "Can be upgraded with 1 Supersonic Boots, 1 Cloud in a Balloon, and 20000 Dark Souls");
-            SimpleModTooltip(Mod, item, tooltips, ItemID.BandofRegeneration, "Can be upgraded with the Band of Starpower and 4000 Dark Souls");
-            SimpleModTooltip(Mod, item, tooltips, ItemID.BandofStarpower, "Can be upgraded with the Band of Regeneration and 4000 Dark Souls");
-            SimpleModTooltip(Mod, item, tooltips, ItemID.CobaltBreastplate, "Set can be upgraded with 9000 Dark Souls");
+            SimpleModTooltip(Mod, item, tooltips, ItemID.AngelWings, "You will discover these in time...", "Can be upgraded with Supersonic Boots");
+            SimpleModTooltip(Mod, item, tooltips, ItemID.BandofRegeneration, "Can be upgraded with the Band of Starpower and Dark Souls");
+            SimpleModTooltip(Mod, item, tooltips, ItemID.BandofStarpower, "Can be upgraded with the Band of Regeneration and Dark Souls");
             SimpleModTooltip(Mod, item, tooltips, ItemID.CobaltDrill, "Use this to gain entry to the Wyvern Mage's", "Fortress above the hallowed caverns to the east");
             SimpleModTooltip(Mod, item, tooltips, ItemID.DivingHelmet, "Can be placed in an accessory slot or in your head slot.");
             SimpleModTooltip(Mod, item, tooltips, ItemID.GoldHelmet, "Can be upgraded with Dark Souls");
-            SimpleModTooltip(Mod, item, tooltips, ItemID.GrapplingHook, "Don't buy or craft this until you have discovered it in the jungle");
             SimpleModTooltip(Mod, item, tooltips, ItemID.HerosHat, "Can be upgraded eventually with the flippers", "and diving helmet, when you acquire them");
-            SimpleModTooltip(Mod, item, tooltips, ItemID.ManaCrystal, "Can be used with 200 Dark Souls to create a Mana Bomb");
+            SimpleModTooltip(Mod, item, tooltips, ItemID.ManaCrystal, "Can be used with Dark Souls to create a Mana Bomb");
             SimpleModTooltip(Mod, item, tooltips, ItemID.MechanicalEye, "Item is non-consumable.");
+            SimpleModTooltip(Mod, item, tooltips, ItemID.MechanicalSkull, "Item is non-consumable.");
             SimpleModTooltip(Mod, item, tooltips, ItemID.MechanicalWorm, "It's heavier than you expected.\nYou get the feeling a way to stay in the air may be key...\nItem is non-consumable.");
             SimpleModTooltip(Mod, item, tooltips, ItemID.MeteorSuit, "Can be augmented with Souls of Light");
-            SimpleModTooltip(Mod, item, tooltips, ItemID.MoltenFury, "Can be upgraded with 1 Soul of Sight and 70,000 Dark Souls");
-            SimpleModTooltip(Mod, item, tooltips, ItemID.MoltenPickaxe, "Use this to open the Cobalt Gate to the east of", "the jungle ruins after you defeat the Wall of Flesh");
-            SimpleModTooltip(Mod, item, tooltips, ItemID.MythrilChainmail, "This armor set can be upgraded with 9000 Dark Souls");
-            SimpleModTooltip(Mod, item, tooltips, ItemID.ShadowHelmet, "Can be upgraded with 1000 Dark Souls");
+            SimpleModTooltip(Mod, item, tooltips, ItemID.MoltenFury, "Can be upgraded with 1 Soul of Sight and Dark Souls");
+            SimpleModTooltip(Mod, item, tooltips, ItemID.Pwnhammer, "Use this to open the Cobalt Gate to the east of", "the jungle ruins after you defeat the Wall of Flesh");
+            SimpleModTooltip(Mod, item, tooltips, ItemID.MythrilChainmail, "This armor set can be upgraded with Dark Souls");
+            SimpleModTooltip(Mod, item, tooltips, ItemID.ShadowHelmet, "Can be upgraded with Dark Souls");
             SimpleModTooltip(Mod, item, tooltips, ItemID.SilverWatch, "Can be upgraded with Dark Souls to change day to night.");
             SimpleModTooltip(Mod, item, tooltips, ItemID.StickyBomb, "More fun to use than a pickaxe!");
             SimpleModTooltip(Mod, item, tooltips, ItemID.WireCutter, "Do not use this!");
@@ -75,11 +74,11 @@ namespace tsorcRevamp.Items.VanillaItems
             SimpleModTooltip(Mod, item, tooltips, ItemID.YellowWrench, "Do not use this!");
             SimpleModTooltip(Mod, item, tooltips, ItemID.MulticolorWrench, "Do not use this!");
             SimpleModTooltip(Mod, item, tooltips, ItemID.CopperAxe, "All axes do 2x damage to woody enemies");
-            SimpleModTooltip(Mod, item, tooltips, ItemID.DemonBow, "Can be upgraded with 4000 Dark Souls", "and 10 Shadow Scales or 10 Tissue Samples");
+            SimpleModTooltip(Mod, item, tooltips, ItemID.DemonBow, "Can be upgraded with Dark Souls", "and 10 Shadow Scales or 10 Tissue Samples");
             SimpleModTooltip(Mod, item, tooltips, ItemID.Diamond, "Vital ingredient in the crafting of a very powerful potion");
             SimpleModTooltip(Mod, item, tooltips, ItemID.IronOre, "Perhaps you can use these for making special arrows..?");
-            SimpleModTooltip(Mod, item, tooltips, ItemID.FeralClaws, "Can be upgraded with 3000 Dark Souls, an Aglet and an Anklet of the Wind");
-            SimpleModTooltip(Mod, item, tooltips, ItemID.Revolver, "Can be upgraded with 6000 Dark Souls and 10 Souls of Light or Dark");
+            SimpleModTooltip(Mod, item, tooltips, ItemID.FeralClaws, "Can be upgraded with Dark Souls, an Aglet, and an Anklet of the Wind");
+            SimpleModTooltip(Mod, item, tooltips, ItemID.Revolver, "Can be upgraded with Dark Souls and 10 Souls of Light or Dark");
 
 
             Player player = Main.LocalPlayer;
