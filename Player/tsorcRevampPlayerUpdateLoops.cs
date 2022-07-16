56ae56c3fa9a6007ddca157aaca8bb4f164afc08
timhjersted <88109305+timhjersted@users.noreply.github.com>
2022-07-16 00:50:22 -0500

    Small tip edits

--- Player/tsorcRevampPlayerUpdateLoops.cs
+++ Player/tsorcRevampPlayerUpdateLoops.cs
@@ -182,6 +182,7 @@ namespace tsorcRevamp
 
          = new List<string>() {
              "The dodgeroll makes you immune to attacks. Mastering it is necessary for survival!",
+             "The dodgeroll key can be changed in controls / mod config. Left Alt by default. Left Shift or Right Click are also nice",
              "Crystal shards often light the path to powerful secrets...",
              "Using potions during normal combat is often necessary to survive",
              "Losing souls on death can be disabled in the config",
@@ -190,13 +191,13 @@ namespace tsorcRevamp
              "Check if any of your items can be upgraded with Dark Souls",
              "Progression clues can be found on unique items dropped by bosses",
              "If you feel lost, seek the dark places on your map. There are secrets and paths in almost every direction",
-             "Signs offer important hints",
+             "Signs offer many important hints",
              "Teal Pressure Pads can only be activated with ranged attacks",
              "Some vanilla recipes have been removed. You must find these items in the world by exploring",
              "Get a bad modifier on a weapon? Talk to Jade, the Emerald Herald. She can remove it with her Blessing",
-             "Grey Stone Gates cannot be broken and must be opened by a lever or switch",
-             "You may be encounter bosses before you can defeat them. If in doubt, continue exploring, level up your gear, and come back when you're stronger",
-             "Play with the Recipe Browser mod to easily see if loot dropped from enemies or found in chests can be crafted into something else",
+             //"Grey Stone Gates cannot be broken and must be opened by a lever or switch",
+             "You may be encounter bosses before you can defeat them. If in doubt, continue exploring, craft new gear, and come back when you're stronger",
+             "Play with the Recipe Browser mod to easily see what your loot can be transformed into",
              "Welcome to the Dark Souls of Terraria",
             };
 
