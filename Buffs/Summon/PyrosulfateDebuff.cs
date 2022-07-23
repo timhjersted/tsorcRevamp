6681b8d355c8f8cd733715b1290efa5919490214
MarfMaster <marfmaster6@gmail.com>
2022-07-23 21:44:35 +0200

    Added Pyrosulfate, fixed Dominatrix crit chance

--- Buffs/Summon/PyrosulfateDebuff.cs
+++ Buffs/Summon/PyrosulfateDebuff.cs
@@ -5,7 +5,7 @@ using Terraria.ModLoader;
 
 namespace tsorcRevamp.Buffs.Summon
 {
-	public class DominatrixDebuff : ModBuff
+	public class PyrosulfateDebuff : ModBuff
 	{
 		public override void SetStaticDefaults()
 		{
@@ -16,29 +16,30 @@ namespace tsorcRevamp.Buffs.Summon
 
 		public override void Update(NPC npc, ref int buffIndex)
 		{
-			npc.GetGlobalNPC<DominatrixDebuffNPC>().markedByDominatrix = true;
+			npc.GetGlobalNPC<PyrosulfateDebuffNPC>().markedByPyrosulfate = true;
 		}
 	}
 
-	public class DominatrixDebuffNPC : GlobalNPC
+	public class PyrosulfateDebuffNPC : GlobalNPC
 	{
 		// This is required to store information on entities that isn't shared between them.
 		public override bool InstancePerEntity => true;
 
-		public bool markedByDominatrix;
+		public bool markedByPyrosulfate;
 
 		public override void ResetEffects(NPC npc)
 		{
-			markedByDominatrix = false;
+			markedByPyrosulfate = false;
 		}
 
 		// TODO: Inconsistent with vanilla, increasing damage AFTER it is randomised, not before. Change to a different hook in the future.
 		public override void ModifyHitByProjectile(NPC npc, Projectile projectile, ref int damage, ref float knockback, ref bool crit, ref int hitDirection)
 		{
 			// Only player attacks should benefit from this buff, hence the NPC and trap checks.
-			if (markedByDominatrix && !projectile.npcProj && !projectile.trap && (projectile.minion || ProjectileID.Sets.MinionShot[projectile.type]))
+			if (markedByPyrosulfate && !projectile.npcProj && !projectile.trap && (projectile.minion || ProjectileID.Sets.MinionShot[projectile.type]))
 			{
-				if (Main.rand.NextBool(20))
+				damage += 8;
+				if (Main.rand.NextBool(33))
 				{
 					crit = true;
 				}
---
+++ Items/Weapons/Summon/Whips/Pyrosulfate.cs
@@ -0,0 +1,55 @@
+/*
+using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace tsorcRevamp.Items.Weapons.Summon.Whips
+{
+	public class Pyrosulfate : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;  //journey mode lmao
+			Tooltip.SetDefault("8 summon tag damage" +
+                "\n3% summon tag critical strike chance" +
+                "\nYour minions will focus struck enemies" +
+                "\nThis whip also inflicts Cursed Inferno");
+		}
+
+		public override void SetDefaults()
+		{
+
+			Item.height = 42;
+			Item.width = 42;
+
+			Item.DamageType = DamageClass.SummonMeleeSpeed;
+			Item.damage = 54;
+			Item.knockBack = 2;
+			Item.rare = ItemRarityID.LightRed;
+
+			Item.shoot = ModContent.ProjectileType<Projectiles.Summon.Whips.PyrosulfateProjectile>();
+			Item.shootSpeed = 4;
+
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.useTime = 30; // for some reason a lower use speed gives it increased range....
+			Item.useAnimation = 30;
+			Item.UseSound = SoundID.Item152;
+			Item.noMelee = true;
+			Item.noUseGraphic = true;
+
+		}
+		public override void AddRecipes()
+		{
+			
+			Terraria.Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.DemoniteBar, 4);
+			recipe.AddIngredient(ItemID.CursedFlame, 20);
+			recipe.AddIngredient(ModContent.ItemType<DarkSoul>(), 10000);
+
+			recipe.AddTile(TileID.DemonAltar);
+			recipe.Register();
+		}
+	}
+}
+*/
