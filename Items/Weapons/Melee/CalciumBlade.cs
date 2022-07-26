ab8c00dd860af6f0fc325bafeae8621b23ded571
MarfMaster <marfmaster6@gmail.com>
2022-07-26 05:29:18 +0200

    Bone Blade rename and on-hit projectile, Jawblade on-hit projectile

--- Items/Weapons/Melee/CalciumBlade.cs
+++ Items/Weapons/Melee/CalciumBlade.cs
@@ -1,37 +1,54 @@
+using Terraria;
+using Microsoft.Xna.Framework;
 using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace tsorcRevamp.Items.Weapons.Melee
 {
-    public class BoneBlade : ModItem
+    [LegacyName("BoneBlade")]
+    public class CalciumBlade : ModItem
     {
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Bone Blade");
-            Tooltip.SetDefault("'A blade of sharpened bone.'");
-
+            Tooltip.SetDefault("'A blade of sharpened bone'" +
+                "\nShoots out a bone upon hitting enemies with the blade");
         }
+        public bool canitshoot = false;
 
         public override void SetDefaults()
         {
-
-
-
             Item.width = 61;
             Item.height = 74;
             Item.useStyle = ItemUseStyleID.Swing;
-            Item.autoReuse = true;
             Item.useAnimation = 25;
             Item.useTime = 25;
             Item.maxStack = 1;
-            Item.damage = 30;
-            Item.knockBack = (float)7;
-            Item.scale = (float).9;
+            Item.damage = 33;
+            Item.knockBack = 7f;
+            Item.scale = 0.9f;
             Item.UseSound = SoundID.Item1;
             Item.rare = ItemRarityID.Green;
             Item.value = PriceByRarity.Green_2;
             Item.DamageType = DamageClass.Melee;
-            Item.material = true;
+            Item.shoot = ProjectileID.Bone;
+            Item.shootSpeed = 10f;
+        }
+
+        public override void OnHitNPC(Player player, NPC target, int damage, float knockBack, bool crit)
+        {
+            canitshoot = true;
+        }
+        public override bool CanShoot(Player player)
+        {
+            if (canitshoot == false)
+            {
+                return false;
+            }
+            else
+            {
+                canitshoot = false;
+                return true;
+            }
         }
 
         public override void AddRecipes()
--- Items/Weapons/Melee/Jawblade.cs
+++ Items/Weapons/Melee/Jawblade.cs
@@ -1,5 +1,8 @@
-﻿using Terraria.ID;
+﻿using Terraria;
+using Microsoft.Xna.Framework;
+using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.DataStructures;
 
 namespace tsorcRevamp.Items.Weapons.Melee
 {
@@ -7,8 +10,11 @@ namespace tsorcRevamp.Items.Weapons.Melee
     {
         public override void SetStaticDefaults()
         {
-            Tooltip.SetDefault("A blade of bone and fangs");
+            Tooltip.SetDefault("'A blade of bone and fangs'" +
+                "\nShoots out a homing skull upon hitting enemies with the blade");
         }
+        public bool canitshoot = false;
+
         public override void SetDefaults()
         {
             Item.width = 68;
@@ -16,22 +22,39 @@ namespace tsorcRevamp.Items.Weapons.Melee
             Item.useStyle = ItemUseStyleID.Swing;
             Item.useAnimation = 30;
             Item.useTime = 30;
-            Item.damage = 46;
+            Item.damage = 56;
             Item.knockBack = 7;
             Item.scale = 1f;
             Item.UseSound = SoundID.Item1;
             Item.rare = ItemRarityID.Orange;
             Item.value = PriceByRarity.Orange_3;
             Item.DamageType = DamageClass.Melee;
-            Item.autoReuse = true;
+            Item.shoot = ProjectileID.BookOfSkullsSkull;
+            Item.shootSpeed = 10f;
+        }
+        public override void OnHitNPC(Player player, NPC target, int damage, float knockBack, bool crit)
+        {
+            canitshoot = true;
+        }
+        public override bool CanShoot(Player player)
+        {
+            if (canitshoot == false)
+            {
+                return false;
+            }
+            else
+            {
+                canitshoot = false;
+                return true;
+            }
         }
 
         public override void AddRecipes()
         {
             Terraria.Recipe recipe = CreateRecipe();
 
-            recipe.AddIngredient(ModContent.ItemType<Items.Weapons.Melee.BoneBlade>());
-            recipe.AddIngredient(Mod.Find<ModItem>("DarkSoul").Type, 3000);
+            recipe.AddIngredient(ModContent.ItemType<CalciumBlade>());
+            recipe.AddIngredient(Mod.Find<ModItem>("DarkSoul").Type, 10000);
 
             recipe.AddTile(TileID.DemonAltar);
 
