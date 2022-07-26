6b58b20c8ecda794c90615071a956b2de15fd8a1
MarfMaster <marfmaster6@gmail.com>
2022-07-26 08:30:25 +0200

    attempt at minion with dmg stacking mechanic and falloff through timers
    waaaaaaaaaaa

---
+++ Items/Weapons/Summon/PhoenixStaff.cs
@@ -0,0 +1,65 @@
+/*
+using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace tsorcRevamp.Items.Weapons.Summon
+{
+	public class PhoenixStaff : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			Tooltip.SetDefault("Summons a raging Phoenix to fight for you");
+
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+			ItemID.Sets.GamepadWholeScreenUseRange[Item.type] = true; // This lets the player target anywhere on the whole screen while using a controller
+			ItemID.Sets.LockOnIgnoresCollision[Item.type] = true;
+		}
+
+		public int ragedamagemultiplier = 2;
+		public override void SetDefaults()
+		{
+			Item.damage = 20 * ragedamagemultiplier;
+			Item.knockBack = 3f;
+			Item.mana = 10;
+			Item.width = 32;
+			Item.height = 32;
+			Item.useTime = 36;
+			Item.useAnimation = 36;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.value = Item.sellPrice(gold: 30);
+			Item.rare = ItemRarityID.Cyan;
+			Item.UseSound = SoundID.Item44;
+
+
+			Item.noMelee = true;
+			Item.DamageType = DamageClass.Summon;
+			Item.buffType = ModContent.BuffType<Buffs.Summon.PhoenixBuff>();
+			Item.shoot = ModContent.ProjectileType<Projectiles.Summon.PhoenixProjectile>();
+		}
+
+		public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+		{
+			// Here you can change where the minion is spawned. Most vanilla minions spawn at the cursor position
+			position = Main.MouseWorld;
+		}
+
+		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+		{
+			// This is needed so the buff that keeps your minion alive and allows you to despawn it properly applies
+			player.AddBuff(Item.buffType, 2);
+
+			// Minions have to be spawned manually, then have originalDamage assigned to the damage of the summon item
+			var projectile = Projectile.NewProjectileDirect(source, position, velocity, type, damage, knockback, Main.myPlayer);
+			projectile.originalDamage = Item.damage;
+
+			// Since we spawned the projectile manually already, we do not need the game to spawn it for ourselves anymore, so return false
+			return false;
+		}
+	}
+}
+*/
