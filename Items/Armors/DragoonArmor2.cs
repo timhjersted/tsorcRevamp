c4920cd1401337fd5c8be8d3c96fd9116c7dadb5
MarfMaster <marfmaster6@gmail.com>
2022-08-01 07:43:48 +0200

    s dragoon dust

--- Items/Armors/DragoonArmor2.cs
+++ Items/Armors/DragoonArmor2.cs
@@ -41,7 +41,7 @@ namespace tsorcRevamp.Items.Armors
                 player.manaRegenBuff = true;
                 player.GetCritChance(DamageClass.Magic) += 3;
                 player.GetDamage(DamageClass.Magic) += .05f;
-                int dust = Dust.NewDust(new Vector2((float)player.position.X, (float)player.position.Y), player.width, player.height, 21, (player.velocity.X) + (player.direction * 1), player.velocity.Y, 245, Color.White, 2.0f);
+                int dust = Dust.NewDust(new Vector2(player.position.X, player.position.Y), player.width, player.height, 21, (player.velocity.X) + (player.direction * 1), player.velocity.Y, 150, Color.White, 1f);
                 Main.dust[dust].noGravity = true;
             }
         }
