fb9568abe3e78b28bbb6833cbef2145205543a6a
RecursiveCollapse <recursivecollapse@protonmail.com>
2022-09-12 19:51:34 -0400

    Signs now apply the Story Time buff
    Functions when the player is within reading range of them

---
+++ Tiles/tsorcGlobalTile.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace tsorcRevamp.Tiles
+{
+    internal class tsorcGlobalTile : GlobalTile
+    {
+
+
+        public override void NearbyEffects(int i, int j, int type, bool closer)
+        {
+            if(type == TileID.Signs)
+            {
+                Player player = Main.LocalPlayer;
+                Vector2 pos = new Vector2(i + 0.5f, j); // the + .5f makes the effect reach from equal distance to left and right
+                float distance = Math.Abs(Vector2.Distance(player.Center, (pos * 16)));
+
+                if (!player.dead && distance <= 120f)
+                {
+                    player.AddBuff(ModContent.BuffType<Buffs.StoryTime>(), 30);
+                }
+            }
+        }
+    }
+}
