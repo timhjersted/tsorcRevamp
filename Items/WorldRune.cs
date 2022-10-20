20036825ed1856bd76f48d2f8271e2965d3ff976
MarfMaster <marfmaster6@gmail.com>
2022-10-20 15:00:33 +0200

    World Rune addition

---
+++ Items/WorldRune.cs
@@ -0,0 +1,25 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace tsorcRevamp.Items
+{
+    class WorldRune : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("An otherworldy artifact of great power" +
+                "\nIt seems to react to dark souls...");
+
+            ItemID.Sets.ItemNoGravity[Item.type] = true;
+        }
+        public override void SetDefaults()
+        {
+            Item.width = 38;
+            Item.height = 42;
+            Item.maxStack = 999;
+            Item.value = 250000;
+            Item.rare = ItemRarityID.Red;
+        }
+    }
+}
