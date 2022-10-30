d85b0249c8958591a8a2f22dd2718d81aa72a22e
timhjersted <88109305+timhjersted@users.noreply.github.com>
2022-10-30 02:12:43 -0500

    12.2 for real

--- Items/BossItems/AaronsProtectionStone.cs
+++ Items/BossItems/AaronsProtectionStone.cs
@@ -12,7 +12,7 @@ namespace tsorcRevamp.Items.BossItems
             Tooltip.SetDefault("The volcanic stoned etched in Aaron's image" +
                                 "\nSaid to protect the one who carries it in times of despair" +
                                 "\n[c/00ffd4:It's true use may reveal itself in time...or at a particular location..." +
-                                "\Not MP compatible currently");
+                                "\nNot MP compatible currently");
             //"\n[c/00ffd4:Use this at the top of The Temple Shrine of The Wall] if your first attempt" +
             //"\ndoes not succeed. (Use it, don't drop it in lava.)"); //+
             //"\nBut first: save, quit and reload before each time you resummon him.");
