33f8c68aee3f246183cb83786aeb2bc4a5b5cfc9
MarfMaster <marfmaster6@gmail.com>
2023-01-08 22:40:31 +0100

    Empty Trophy for later

---
+++ Items/Placeable/Trophies/EmptyTrophy.cs
@@ -0,0 +1,28 @@
+using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace tsorcRevamp.Items.Placeable.Trophies
+{
+	public class EmptyTrophy : ModItem
+	{
+		public override void SetStaticDefaults() 
+		{
+			DisplayName.SetDefault("Empty Trophy");
+
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+
+		public override void SetDefaults() 
+		{
+			Item.DefaultToPlaceableTile(ModContent.TileType<Tiles.Trophies.EmptyTrophyTile>());
+
+			Item.width = 32;
+			Item.height = 32;
+			Item.maxStack = 99;
+			Item.rare = ItemRarityID.Blue;
+			Item.value = Item.buyPrice(0, 15, 0, 0);
+		}
+	}
+}
