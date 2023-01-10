63f4121bcfec74849c6d0f6412596a9c8da2222c
MarfMaster <marfmaster6@gmail.com>
2023-01-10 07:31:50 +0100

    Made Supreme Mana Potion only usable through quick mana
    REEEEEEEEEEEEEEEEEE
    At least now players can't break the game?

--- Items/Potions/SupremeManaPotion.cs
+++ Items/Potions/SupremeManaPotion.cs
@@ -9,15 +9,13 @@ namespace tsorcRevamp.Items.Potions
         public override void SetStaticDefaults()
         {
             Tooltip.SetDefault("Restores 400 mana" +
-                "\nApplies 3 seconds of mana sickness");
+                "\nApplies 3 seconds of mana sickness" +
+                "\nOnly usable through quick mana");
         }
         public override void SetDefaults()
         {
             Item.width = 20;
             Item.height = 26;
-            Item.useStyle = ItemUseStyleID.DrinkLiquid;
-            Item.useAnimation = 15;
-            Item.useTime = 15;
             Item.maxStack = 9999;
             Item.rare = ItemRarityID.Green;
             Item.consumable = true;
@@ -26,17 +24,8 @@ namespace tsorcRevamp.Items.Potions
         }
         public override bool? UseItem(Player player)    
         {
-            int index = player.FindBuffIndex(BuffID.ManaSickness);
-            if (!player.HasBuff(BuffID.ManaSickness))
-            {
-                player.AddBuff(BuffID.ManaSickness, 14);
-            }
-            else
-            {
-                player.buffTime[index] += 14;
-            }
             player.statMana += 400;
-            return base.UseItem(player);
+            return true;
         }
         public override void AddRecipes()
         {
