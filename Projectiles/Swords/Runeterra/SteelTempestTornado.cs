e71d58b9fb051e247880d687c48f86de59260da2
MarfMaster <marfmaster6@gmail.com>
2023-01-20 06:00:19 +0100

    Steel Tempest sound fix and beetle idol sprite

--- Projectiles/Swords/Runeterra/SteelTempestTornado.cs
+++ Projectiles/Swords/Runeterra/SteelTempestTornado.cs
@@ -3,6 +3,7 @@ using Terraria.Audio;
 using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
+using ReLogic.Utilities;
 
 namespace tsorcRevamp.Projectiles.Swords.Runeterra
 {
@@ -20,23 +21,53 @@ namespace tsorcRevamp.Projectiles.Swords.Runeterra
             Projectile.friendly = true;
             Projectile.tileCollide = false;
             Projectile.penetrate = -1;
-            Projectile.timeLeft = 800;
+            Projectile.timeLeft = 300;
             Projectile.DamageType = DamageClass.Melee;
         }
+        SlotId SoundSlotID;
+        SoundStyle TornadoSoundStyle = SoundID.DD2_BookStaffTwisterLoop;
+        bool soundPaused;
+        bool playedSound = false;
+        ActiveSound TornadoSound;
         public override void OnSpawn(IEntitySource source)
         {
-            SoundEngine.PlaySound(SoundID.DD2_BookStaffTwisterLoop, Projectile.Center);
             Player player = Main.player[Projectile.owner];
             Projectile.damage = (int)(player.GetWeaponDamage(player.HeldItem) * (1.5f + player.GetTotalAttackSpeed(DamageClass.Melee))); //scales with attack speed since higher melee speed increases the travelling speed, therefore giving it less time to get hits off
         }
-        public override void Kill(int timeLeft)
-        {
-            SoundEngine.FindActiveSound(SoundID.DD2_BookStaffTwisterLoop);
-            SoundEngine.StopTrackedSounds();
-        }
+
         public override void AI()
         {
+            if (!playedSound)
+            {
+                playedSound = true;
+                SoundSlotID = SoundEngine.PlaySound(SoundID.DD2_BookStaffTwisterLoop, Projectile.Center); //can give funny pitch hehe
+                if (TornadoSound == null)
+                {
+                    SoundEngine.TryGetActiveSound(SoundSlotID, out TornadoSound);
+                }
+                else
+                {
+                    if (SoundEngine.AreSoundsPaused && !soundPaused)
+                    {
+                        TornadoSound.Pause();
+                        soundPaused = true;
+                    }
+                    else if (!SoundEngine.AreSoundsPaused && soundPaused)
+                    {
+                        TornadoSound.Resume();
+                        soundPaused = false;
+                    }
+                    TornadoSound.Position = Projectile.Center;
+                }
+            }
             Visuals();
+        }
+        public override void Kill(int timeLeft)
+        {
+            if (Projectile.timeLeft < 2)
+            {
+                TornadoSound.Stop();
+            }
         }
         private void Visuals()
         {
