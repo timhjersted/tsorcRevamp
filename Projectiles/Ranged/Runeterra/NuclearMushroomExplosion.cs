575103688f345d1fd1582e9d2e84341305ea5b7c
Marf <108209641+MarfMaster@users.noreply.github.com>
2023-04-02 00:26:29 +0200

    last commit before 1.4.4 from me

--- Projectiles/Ranged/Runeterra/NuclearMushroomExplosion.cs
+++ Projectiles/Ranged/Runeterra/NuclearMushroomExplosion.cs
@@ -6,27 +6,28 @@ using tsorcRevamp.Buffs.Runeterra.Ranged;
 
 namespace tsorcRevamp.Projectiles.Ranged.Runeterra
 {
-	public class NuclearMushroom: ModProjectile
+	public class NuclearMushroomExplosion: ModProjectile
 	{
 		public override void SetStaticDefaults()
 		{
-			DisplayName.SetDefault("Nuclear Mushroom");
+			DisplayName.SetDefault("Nuclear Mushroom Explosion");
 			ProjectileID.Sets.TrailCacheLength[Projectile.type] = 5; // The length of old position to be recorded
 			ProjectileID.Sets.TrailingMode[Projectile.type] = 0; // The recording mode
-		}
+            Main.projFrames[Projectile.type] = 16;
+        }
 
 		public override void SetDefaults()
 		{
-			Projectile.width = 50;
-			Projectile.height = 50;
+			Projectile.width = 500;
+			Projectile.height = 500;
 
 			Projectile.friendly = true;
 			Projectile.DamageType = DamageClass.Ranged;
-			Projectile.penetrate = 1;
-			Projectile.timeLeft = 100 * 60;
+			Projectile.penetrate = -1;
+			Projectile.timeLeft = 10 * 60;
 			Projectile.ignoreWater = true;
-			Projectile.tileCollide = true;
-			Projectile.extraUpdates = 3;
+			Projectile.tileCollide = false;
+			Projectile.extraUpdates = 0;
             Projectile.knockBack = 10f;
 
 		}
@@ -36,11 +37,31 @@ namespace tsorcRevamp.Projectiles.Ranged.Runeterra
             Player owner = Main.player[Projectile.owner];
             Projectile.CritChance = owner.GetWeaponCrit(owner.HeldItem);
             Dust.NewDust(Projectile.TopLeft, 20, 20, DustID.GlowingMushroom, 0, 0, 250, Color.Brown, 0.25f);
-
+            Visuals();
         }
         public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
         {
-			Projectile.NewProjectile(Projectile.GetSource_None(), Projectile.Center, Vector2.Zero, ModContent.ProjectileType<NuclearMushroomExplosion>(), Projectile.damage * 10, Projectile.knockBack * 10f);
+			target.AddBuff(ModContent.BuffType<IrradiatedByShroomDebuff>(), 600);
+        }
+        private void Visuals()
+        {
+            int frameSpeed = 5;
+
+            Projectile.frameCounter++;
+
+            if (Projectile.frameCounter >= frameSpeed)
+            {
+                Projectile.frameCounter = 0;
+                Projectile.frame++;
+
+                if (Projectile.frame >= Main.projFrames[Projectile.type])
+                {
+                    Projectile.frame = 0;
+                }
+            }
+
+            // Some visuals here
+            Lighting.AddLight(Projectile.Center, Color.GreenYellow.ToVector3() * 10f);
         }
     }
 }
