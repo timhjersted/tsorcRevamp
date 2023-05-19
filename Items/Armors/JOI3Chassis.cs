c8c70405ab72d6546e938340cd97e6c4f5fef689
Marf <108209641+MarfMaster@users.noreply.github.com>
2023-05-20 00:00:27 +0200

    JOI3's Suit

---
+++ Items/Armors/JOI3Chassis.cs
@@ -0,0 +1,50 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace tsorcRevamp.Items.Armors
+{
+    [AutoloadEquip(EquipType.Body)]
+    public class JOI3Chassis : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            int equipslot = EquipLoader.GetEquipSlot(Mod, "JOI3Chassis_Legs", EquipType.Legs);
+            ArmorIDs.Legs.Sets.OverridesLegs[equipslot] = true;
+            ArmorIDs.Legs.Sets.HidesBottomSkin[equipslot] = true;
+            ArmorIDs.Legs.Sets.HidesTopSkin[equipslot] = true;
+            ArmorIDs.Legs.Sets.IncompatibleWithFrogLeg[equipslot] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.vanity = true;
+            Item.width = 36;
+            Item.height = 32;
+            Item.rare = ItemRarityID.Master;
+            Item.value = PriceByRarity.fromItem(Item);
+        }
+        public override void Load()
+        {
+            if (Main.netMode == NetmodeID.Server)
+                return;
+            EquipLoader.AddEquipTexture(Mod, "tsorcRevamp/Items/Armors/JOI3Chassis_Legs", EquipType.Legs, this, "JOI3Chassis_Legs");
+        }
+        public override void SetMatch(bool male, ref int equipSlot, ref bool robes)
+        {
+            robes = true;
+            equipSlot = EquipLoader.GetEquipSlot(Mod, "JOI3Chassis_Legs", EquipType.Legs);
+        }
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.CopperBar, 5);
+            recipe.AddIngredient(ItemID.IronBar, 5);
+            recipe.AddIngredient(ItemID.SilverBar, 5);
+            recipe.AddIngredient(ItemID.GoldBar, 5);
+            recipe.AddTile(TileID.Anvils);
+
+            recipe.Register();
+        }
+    }
+}
---
+++ Items/Armors/JOI3Headgear.cs
@@ -0,0 +1,34 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace tsorcRevamp.Items.Armors
+{
+    [AutoloadEquip(EquipType.Head)]
+    public class JOI3Headgear : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+        }
+
+        public override void SetDefaults()
+        {
+            Item.vanity = true;
+            Item.width = 30;
+            Item.height = 18;
+            Item.rare = ItemRarityID.Master;
+            Item.value = PriceByRarity.fromItem(Item);
+        }
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.CopperBar, 5);
+            recipe.AddIngredient(ItemID.IronBar, 5);
+            recipe.AddIngredient(ItemID.SilverBar, 5);
+            recipe.AddIngredient(ItemID.GoldBar, 5);
+            recipe.AddTile(TileID.Anvils);
+
+            recipe.Register();
+        }
+    }
+}
