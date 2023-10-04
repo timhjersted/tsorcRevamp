3387d7a46017c300654890dde90f616fff5bb845
RecursiveCollapse <recursivecollapse@protonmail.com>
2023-10-04 14:08:18 -0400

    Hallow upgrades recipes
    They can now use either kind of hallowed armor

--- Items/Armors/Magic/AnkorWatLeggings.cs
+++ Items/Armors/Magic/AnkorWatLeggings.cs
@@ -42,6 +42,14 @@ namespace tsorcRevamp.Items.Armors.Magic
             recipe.AddTile(TileID.DemonAltar);
 
             recipe.Register();
+
+
+            Recipe recipe2 = CreateRecipe();
+            recipe2.AddIngredient(ItemID.AncientHallowedGreaves, 1);
+            recipe2.AddIngredient(ModContent.ItemType<DarkSoul>(), 20000);
+            recipe2.AddTile(TileID.DemonAltar);
+
+            recipe2.Register();
         }
     }
 }
