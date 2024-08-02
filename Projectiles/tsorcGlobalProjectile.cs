6d2f0de76f4af1e65992bfc1ada5207334bb56ad
Silverune7 <Silverune7@yahoo.com>
2024-08-02 15:21:32 -0600

    Update tsorcGlobalProjectile.cs
    Adjustments to Babyslime, Flinx, BabyBird, and Vampirefrog summons. 
    
    Babyslime will get slow debuff
    Flinx getting Frostburn, lower base damage
    Babybird, lowered damage but doubling the amount summoned
    Vampirefrog, damage reduced and knockback but getting onhit effect to heal

--- Projectiles/tsorcGlobalProjectile.cs
+++ Projectiles/tsorcGlobalProjectile.cs
@@ -398,6 +398,22 @@ namespace tsorcRevamp.Projectiles
                 target.AddBuff(ModContent.BuffType<Buffs.CrescentMoonlight>(), 3 * 60); // Adds the ExampleJavelin debuff for a very small DoT
             }
 
+            if (projectile.type == ProjectileID.BabySlime)
+            {
+                target.AddBuff(BuffID.Slow, 60);
+            }
+
+            if (projectile.type == ProjectileID.FlinxMinion)
+            {
+                target.AddBuff(BuffID.Frostburn, 60);
+            }
+            
+            if (projectile.type == ProjectileID.VampireFrog)
+            {
+                player.statLife += modifiers.GetDamage(modifiers.SourceDamage.ApplyTo(Item.damage) / 1, true);
+                player.HealEffect(modifiers.GetDamage(modifiers.SourceDamage.ApplyTo(Item.damage) / 1, true));
+
+
             if (projectile.owner == Main.myPlayer && !projectile.hostile && modPlayer.MiakodaNewBoost && projectile.type != (int)ModContent.ProjectileType<MiakodaNew>())
             {
                 target.AddBuff(BuffID.Midas, 5 * 60);
