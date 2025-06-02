c44af205e5aa8072ee2cb41737b8901f4e752d99
Xelvaa <seb.gervat@gmail.com>
2025-06-03 00:46:29 +0200

    Increased Pilgrim's Spontoon size and fixed the tooltip

--- Projectiles/Melee/Spears/PilgrimSpontoonProj.cs
+++ Projectiles/Melee/Spears/PilgrimSpontoonProj.cs
@@ -10,10 +10,10 @@ namespace tsorcRevamp.Projectiles.Melee.Spears
 {
     class PilgrimSpontoonProj : ModdedSpearProjectile
     {
-        public override float HoldoutRangeMin => 40f;
-        public override float HoldoutRangeMax => 120f;
-        public override float HitboxSize => 1;
-        public override float Scale => 1.1f;
+        public override float HoldoutRangeMin => 52f;
+        public override float HoldoutRangeMax => 156f;
+        public override float HitboxSize => 1.1f;
+        public override float Scale => 1;
         public override int dustID => DustID.UnusedWhiteBluePurple;
         public override void ModifyHitNPC(NPC target, ref NPC.HitModifiers modifiers)
         {
