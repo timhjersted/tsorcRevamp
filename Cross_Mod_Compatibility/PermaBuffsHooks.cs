f46c86fcb421a4d387e7b9d6d5c85effe29de717
linklight2 <linklight2@gmail.com>
2025-06-07 20:39:48 -0400

    CrossModCompat

---
+++ Cross_Mod_Compatibility/PermaBuffsHooks.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+using tsorcRevamp.Buffs;
+using tsorcRevamp;
+
+namespace PermaBuffs
+{
+    [JITWhenModsEnabled("PermaBuffs")]
+    public partial class PermaBuffsHooks
+    {
+        public static void Curse(Player player, int buffSlotOnPlayer, bool isPermaBuffed, out int buffType)
+        {
+            buffType = ModContent.BuffType<Curse>();
+            if (player == null)
+                return;
+
+            if (!isPermaBuffed)
+            {
+                var modPlayer = player.GetModPlayer<tsorcRevampPlayer>();
+
+                modPlayer.CurseActive = false;
+                modPlayer.powerfulCurseActive = false;
+            }
+        }
+    }
+}
