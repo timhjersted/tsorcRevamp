b50d36ee38ef9bec651b2e9432fa731571375074
Xelvaa <55628418+Xelvaa@users.noreply.github.com>
2026-05-19 11:22:01 +0200

    Merge pull request #83 from DreamSea/modded-spear-projectiles
    have Gaebolg poke/thrown subclass ModdedSpearPojectile*

--- Projectiles/Melee/Spears/GaeBolgPoke.cs
+++ Projectiles/Melee/Spears/GaeBolgPoke.cs
@@ -1,126 +1,36 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using System;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-//using tsorcRevamp.Dusts;
-
 namespace tsorcRevamp.Projectiles.Melee.Spears
 {
-    class GaeBolgPoke : ModProjectile
+    class GaeBolgPoke : ModdedSpearProjectile
     {
+        public override float HoldoutRangeMin => 75f;
+        public override float HoldoutRangeMax => 160f;
+        public override float HitboxSize => 1;
+        public override float Scale => 1.1f;
+        public override int dustID => -2; // skip ModdedSpearProjectile default dust
 
         public override void SetDefaults()
         {
+            base.SetDefaults();
             Projectile.width = 30;
             Projectile.height = 30;
-            Projectile.penetrate = -1;
-            Projectile.timeLeft = 3600;
-            Projectile.friendly = true; //can hit enemies
-            Projectile.hostile = false; //can hit player / friendly NPCs
             Projectile.ownerHitCheck = false;
-            Projectile.DamageType = DamageClass.Melee;
-            Projectile.tileCollide = false;
-            Projectile.hide = true;
             Projectile.usesLocalNPCImmunity = true;
             Projectile.localNPCHitCooldown = 10;
-            Projectile.scale = 1.1f;
-
-        }
-        public float moveFactor
-        { //controls spear speed
-            get => Projectile.ai[0];
-            set => Projectile.ai[0] = value;
         }
 
-        public override void AI()
+        public override void CustomDust()
         {
-            //DrawOffsetX = -35;
-            Player pOwner = Main.player[Projectile.owner];
-            Projectile.Center = pOwner.RotatedRelativePoint(pOwner.MountedCenter, true);
-            Projectile.direction = pOwner.direction;
-            pOwner.heldProj = Projectile.whoAmI;
-            pOwner.itemTime = pOwner.itemAnimation;
-
-            if (!pOwner.frozen)
-            {
-                if (moveFactor == 0f)
-                { //when initially thrown
-                    moveFactor = 4.1f; //move forward
-                    Projectile.netUpdate = true;
-                }
-                if (pOwner.itemAnimation < pOwner.itemAnimationMax / 2)
-                { //after x animation frames, return
-                    moveFactor -= 2.85f;
-                }
-                else
-                { //extend spear
-                    moveFactor += 4.1f;
-                }
-
-            }
-
-            if (pOwner.itemAnimation == 0)
-            {
-                Projectile.Kill();
-            }
-
-            Projectile.Center += Projectile.velocity * moveFactor;
-
-            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2 + MathHelper.ToRadians(45f);
-
             if (Main.rand.NextBool(2))
             {
-                Vector2 spearTipOffset = new Vector2(60 * MathF.Cos(Projectile.velocity.ToRotation()), 100 * MathF.Sin(Projectile.velocity.ToRotation()));
-                int dust = Dust.NewDust(Projectile.position + spearTipOffset, Projectile.width, Projectile.height, 15, Projectile.velocity.X * -0.2f, Projectile.velocity.Y * -0.2f, 70, default(Color), 1.2f);
+                int dust = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, 15, Projectile.velocity.X * -0.2f, Projectile.velocity.Y * -0.2f, 70, default(Color), 1.2f);
                 Main.dust[dust].noGravity = true;
             }
         }
-
-        public override void ModifyDamageHitbox(ref Rectangle hitbox)
-        {
-            // Move damage hitbox to where spear tip is
-            double spearTipOffsetX = 60 * Math.Cos(Projectile.velocity.ToRotation());
-            double spearTipOffsetY = 60 * Math.Sin(Projectile.velocity.ToRotation());
-            hitbox.Offset((int)spearTipOffsetX, (int)spearTipOffsetY);
-        }
-
-		public override void CutTiles()
-		{
-			// Move vine/pot breaking to where spear tip is
-            Vector2 spearTipOffsetStarting = new Vector2(85 * MathF.Cos(Projectile.velocity.ToRotation()), 85 * MathF.Sin(Projectile.velocity.ToRotation()));
-            Vector2 spearTipOffsetEnding = new Vector2(115 * MathF.Cos(Projectile.velocity.ToRotation()), 115 * MathF.Sin(Projectile.velocity.ToRotation()));
-			Utils.PlotTileLine(Projectile.Center + spearTipOffsetStarting, Projectile.Center + spearTipOffsetEnding, 70, DelegateMethods.CutTiles);
-		}
-
-        public static Texture2D texture;
-        public override bool PreDraw(ref Color lightColor)
-        {
-            SpriteEffects spriteEffects = SpriteEffects.None;
-            if (Projectile.spriteDirection == -1)
-            {
-                spriteEffects = SpriteEffects.FlipHorizontally;
-            }
-
-            if (texture == null || texture.IsDisposed)
-            {
-                texture = (Texture2D)ModContent.Request<Texture2D>(Projectile.ModProjectile.Texture);
-            }
-
-            Rectangle sourceRectangle = new Rectangle(0, 0, texture.Width, texture.Height);
-            Vector2 origin = sourceRectangle.Size() / 2f;
-
-            Vector2 offset = Projectile.velocity * 25;
-
-            Main.EntitySpriteDraw(texture,
-                Projectile.Center - Main.screenPosition + new Vector2(DrawOffsetX, Projectile.gfxOffY) - offset,
-                sourceRectangle, lightColor, Projectile.rotation, origin, Projectile.scale, spriteEffects, 0);
-
-            return false;
-        }
-
     }
-
 }
--- Projectiles/Melee/Spears/GaeBolgThrown.cs
+++ Projectiles/Melee/Spears/GaeBolgThrown.cs
@@ -1,79 +1,20 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Terraria;
-using Terraria.Graphics.Shaders;
 using Terraria.ID;
-using Terraria.ModLoader;
 
 
 namespace tsorcRevamp.Projectiles.Melee.Spears
 {
-    class GaeBolgThrown : ModProjectile
+    class GaeBolgThrown : ModdedSpearProjectileThrown
     {
+        public override int HitboxSize => 24;
+        public override int ChargedShaderID => ItemID.StardustDye;
 
-        public override void SetDefaults()
-        {
-            Projectile.aiStyle = -1;
-            Projectile.hostile = false;
-            Projectile.friendly = true;
-            Projectile.height = 24;
-            Projectile.light = 0.6f;
-            Projectile.DamageType = DamageClass.Melee;
-            Projectile.scale = 1.1f;
-            Projectile.penetrate = 4;
-            Projectile.width = 24;
-            Projectile.tileCollide = false;
-            Projectile.timeLeft = 240;
-        }
-
-        public static Texture2D texture;
-        public override bool PreDraw(ref Color lightColor)
-        {
-            Main.spriteBatch.End();
-            Main.spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.LinearClamp, DepthStencilState.None, RasterizerState.CullNone, null, Main.GameViewMatrix.TransformationMatrix);
-
-            if (Projectile.ai[0] == 1)
-            {
-                ArmorShaderData data = GameShaders.Armor.GetSecondaryShader((byte)GameShaders.Armor.GetShaderIdFromItemId(ItemID.StardustDye), Main.LocalPlayer);
-                data.Apply(null);
-            }
-
-            SpriteEffects spriteEffects = SpriteEffects.None;
-            if (Projectile.spriteDirection == -1)
-            {
-                spriteEffects = SpriteEffects.FlipHorizontally;
-            }
-
-            if (texture == null || texture.IsDisposed)
-            {
-                texture = (Texture2D)ModContent.Request<Texture2D>(Projectile.ModProjectile.Texture);
-            }
-
-            int frameHeight = texture.Height / Main.projFrames[Projectile.type];
-            int startY = frameHeight * Projectile.frame;
-            Rectangle sourceRectangle = new Rectangle(0, startY, texture.Width, frameHeight);
-            Vector2 origin = sourceRectangle.Size() / 2f;
-            Main.EntitySpriteDraw(texture,
-                Projectile.Center - Main.screenPosition + new Vector2(0f, Projectile.gfxOffY),
-                sourceRectangle, Color.White, Projectile.rotation, origin, Projectile.scale, spriteEffects, 0);
-
-            UsefulFunctions.RestartSpritebatch(ref Main.spriteBatch);
-
-            return false;
-        }
-
-        public override void AI()
-        {
-            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2 + MathHelper.ToRadians(45f); //This makes it rotate to face where it's moving
-            //projectile.velocity.Y += (9.8f / 60); //This is its gravity. Comes out to about 0.16 per frame, which is actually really high!!
-            Projectile.velocity.Y += 0.1f;
-
-            if (Main.rand.NextBool(4))
-            {
-                int dust = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, 15, Projectile.velocity.X * -0.2f, Projectile.velocity.Y * -0.2f, 70, default(Color), 1.2f);
-                Main.dust[dust].noGravity = true;
-            }
-        }
+        public override float Light => 0.6f;
+        public override int Penetrate => 4;
+        public override float Scale => 1.1f;
+        public override int TimeLeft => 240;
 
         public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
         {
@@ -83,11 +24,29 @@ namespace tsorcRevamp.Projectiles.Melee.Spears
             }
         }
 
+        public override void AIDust()
+        {
+            if (Main.rand.NextBool(4))
+            {
+                int dust = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, 15, Projectile.velocity.X * -0.2f, Projectile.velocity.Y * -0.2f, 70, default(Color), 1.2f);
+                Main.dust[dust].noGravity = true;
+            }
+        }
+
+        public override void PreKillDust()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, 15, 0, 0, 0, default, 1f);
+            }
+        }
+
         public override void PostDraw(Color lightColor)
         {
             Texture2D texture = (Texture2D)Terraria.GameContent.TextureAssets.Projectile[Projectile.type];
             Vector2 origin = new Vector2(texture.Width / 2f, texture.Height / 2f); 
-            Vector2 drawPosition = Projectile.Center - Main.screenPosition;
+            Vector2 spearTipOffset = origin.RotatedBy(Projectile.rotation);
+            Vector2 drawPosition = Projectile.Center - Main.screenPosition + spearTipOffset;
 
             for (int i = 1; i <= 4; i++)  
             {
@@ -106,18 +65,6 @@ namespace tsorcRevamp.Projectiles.Melee.Spears
                     0
                 );
             }
-        }
-
-        public override bool PreKill(int timeleft)
-        {
-            Projectile.type = ProjectileID.WoodenArrowHostile;
-
-            Terraria.Audio.SoundEngine.PlaySound(SoundID.Dig, Projectile.Center);
-            for (int i = 0; i < 10; i++)
-            {
-                Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, 15, 0, 0, 0, default, 1f);
-            }
-            return true;
         }
     }
 }
