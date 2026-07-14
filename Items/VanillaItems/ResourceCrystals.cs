7193e06cab06abb05ab4d24d4719b0c8cd2a9e59
Marf <marfmaster@proton.me>
2026-07-14 13:37:42 +0200

    Fixed game consuming tons of crystals if you already reached max resources

--- Items/VanillaItems/ResourceCrystals.cs
+++ Items/VanillaItems/ResourceCrystals.cs
@@ -8,7 +8,11 @@ public class ResourceCrystals : GlobalItem
 {
     public override bool CanUseItem(Item item, Player player)
     {
-        if ((item.type == ItemID.LifeCrystal | item.type == ItemID.ManaCrystal) && item.stack < 2)
+        if (item.type == ItemID.LifeCrystal && player.statLifeMax < 400 && item.stack < 2)
+        {
+            return false;
+        }
+        if (item.type == ItemID.ManaCrystal && player.statManaMax < 200 && item.stack < 2)
         {
             return false;
         }
@@ -17,7 +21,11 @@ public class ResourceCrystals : GlobalItem
 
     public override bool? UseItem(Item item, Player player)
     {
-        if (item.type == ItemID.LifeCrystal | item.type == ItemID.ManaCrystal)
+        if (item.type == ItemID.LifeCrystal && player.statLifeMax < 400)
+        {
+            item.stack--;
+        }
+        if (item.type == ItemID.ManaCrystal && player.statManaMax < 200)
         {
             item.stack--;
         }
