7c560c4d18ec0662d287b5ac9d8ae1f4d07daa38
Marf <marfmaster@proton.me>
2026-07-19 07:08:42 +0200

    Fixed Caius' pyre and nerfed Beetle Idol slightly
    Caius' Pyre being a magic/summon sentry was intended. Not sure why it's damage was nerfed to shit, but I slightly adjusted its behavior to be closer to the original weapon and harder to use. Also increased the damage to be comparable to Lunar Portal staff, though I'm surprised at how Portal Staff got reverted from having static iframes....

--- Projectiles/Summon/Sentry/CaiusPyreRift.cs
+++ Projectiles/Summon/Sentry/CaiusPyreRift.cs
@@ -14,23 +14,9 @@ namespace tsorcRevamp.Projectiles.Summon.Sentry
         public override int ProjectileHeight => 84;
         public override DamageClass ProjectileDamageType => DamageClass.MagicSummonHybrid;
         public override bool ContactDamage => false;
-        public override bool CanShoot
-        {
-            //Only fire if at least one enemy is within line of sight!
-            get
-            {
-                for (int i = 0; i < Main.npc.Length; i++)
-                {
-                    if (Main.npc[i].active && Collision.CanHitLine(Projectile.Center, 1, 1, Main.npc[i].Center, 1, 1))
-                    {
-                        return true;
-                    }
-                }
-                return false;
-            }
-        }
+        public override bool CanShoot => true;
         public override int ShotProjectileType => ModContent.ProjectileType<CaiusPyreFireball>();
-        public override float ProjectileInitialVelocity => 0;
+        public override float ProjectileInitialVelocity => 5;
         public override bool PlaysSoundOnShot => true;
         public override SoundStyle ShootSoundStyle => SoundID.Item20;
         public override float ShootSoundVolume => 1f;
